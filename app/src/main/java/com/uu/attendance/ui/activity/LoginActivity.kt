2883afd2ea9e69a1592f4769707f00aba6d01986
klxiaoniu <klxiaoniu@proton.me>
2023-08-13 10:53:57 +0800

    登录界面完善

--- app/src/main/java/com/uu/attendance/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/uu/attendance/ui/activity/LoginActivity.kt
@@ -16,6 +16,18 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>() {
                     height = (binding.ivBg.height * 0.125).toInt()
                     binding.spaceTop.layoutParams = this
                 }
+                binding.spaceAvatarUsername.layoutParams.apply {
+                    height = (binding.ivBg.height * 0.1).toInt()
+                    binding.spaceAvatarUsername.layoutParams = this
+                }
+                binding.spaceUsernamePassword.layoutParams.apply {
+                    height = (binding.ivBg.height * 0.055).toInt()
+                    binding.spaceUsernamePassword.layoutParams = this
+                }
+                binding.spacePasswordButton.layoutParams.apply {
+                    height = (binding.ivBg.height * 0.055).toInt()
+                    binding.spacePasswordButton.layoutParams = this
+                }
                 binding.ivBg.viewTreeObserver.removeOnGlobalLayoutListener(this)
             }
         })
