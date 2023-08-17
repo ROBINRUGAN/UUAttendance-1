5f8818b625cc75e8ed50e1365f32abeafb98946b
klxiaoniu <klxiaoniu@proton.me>
2023-08-17 12:12:53 +0800

    修复连续Toast只显示最后一个

--- app/src/main/java/com/uu/attendance/MyApplication.kt
+++ app/src/main/java/com/uu/attendance/MyApplication.kt
@@ -1,6 +1,7 @@
 package com.uu.attendance
 
 import android.app.Application
+import com.hjq.toast.ToastStrategy
 import com.hjq.toast.Toaster
 import com.hjq.toast.style.WhiteToastStyle
 
@@ -9,10 +10,12 @@ class MyApplication : Application() {
         lateinit var instance: MyApplication
             private set
     }
+
     override fun onCreate() {
         super.onCreate()
         instance = this
         Toaster.init(this)
         Toaster.setStyle(WhiteToastStyle())
+        Toaster.setStrategy(ToastStrategy(ToastStrategy.SHOW_STRATEGY_TYPE_QUEUE))
     }
 }
